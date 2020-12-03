bf7729190b5d4a346265e6476ca2eecc0c35174a
Mike <ms43004foru@gmail.com>
2020-12-03 17:12:49 -0500

    init commit

--- WeatherMap/Program.cs
+++ WeatherMap/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
 
 namespace WeatherMap
 {
@@ -6,7 +8,16 @@ namespace WeatherMap
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string key = File.ReadAllText("appsettings.json");
+            string APIkey = JObject.Parse(key).GetValue("DefaultKey").ToString();
+            Console.WriteLine("Please enter your zipcode:");
+            string zipCode = Console.ReadLine();
+            Console.Write("Please enter your country code:");
+            string countryCode = Console.ReadLine().ToLower();
+            string apiCall = $"https://api.openweathermap.org/data/2.5/weather?zip={zipCode},{countryCode}&appid={APIkey}";
+            WeatherMap.GetTemp(apiCall);
+            Console.WriteLine(WeatherMap.GetTemp(apiCall));
         }
     }
+    
 }
---
+++ WeatherMap/WeatherMap.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace WeatherMap
+{
+    class WeatherMap
+    {
+        public static double GetTemp(string apiCall)
+        {
+            var client = new HttpClient();
+
+            var response = client.GetStringAsync(apiCall).Result;
+            var kelvin = double.Parse(JObject.Parse(response)["main"]["temp"].ToString());
+            return (kelvin - 273.15) * 9 / 5 + 32;
+        }
+    }
+}
